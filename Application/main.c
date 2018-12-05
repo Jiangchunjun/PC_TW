5b7dd54703d1c375b761d4946b84ada5b2dd418a
Jiangchunjun <279521256@qq.com>
2018-12-05 10:49:16 +0800

    update test
    no update

--- Application/main.c
+++ Application/main.c
@@ -105,7 +105,7 @@ void main(void)
                     } 
                   }
                 }
-                CURRENT_UPDATE_DUTY(g_a_duty);
+                CURRENT_UPDATE_DUTY(g_a_duty);//update duty
                 VOLTAGE_UPDATE_DUTY(g_a_duty);  
             }
         }
